7e6ef3f0b1cbe13363553ae1fab195baa2f9b05a
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-08-08 22:26:15 +0200

    Add routes search and about

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/navbar/Navbar'
 import { Home } from './pages/home/Home'
 import { Browse } from './pages/browse/Browse'
+import { Search } from './pages/search/Search'
+import { About } from './pages/about/About'
 
 function App() {
 
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/browse' element={<Browse />}></Route>
+          <Route path='/search' element={<Search />}></Route>
+          <Route path='/about' element={<About />}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import companyLogo from "../../assets/logo.png"
 
 export const Navbar = () => {
     return (
         <div className='navbar'>
+            <div className="logo-container">
+                <img src={companyLogo} alt="" width={100} />
+            </div>
             <Link to={"/"}>Home</Link>
             <Link to={"/browse"}>Browse</Link>
+            <Link to={"/search"}>Search</Link>
+            <Link to={"/about"}>About</Link>
         </div>
     )
 }
