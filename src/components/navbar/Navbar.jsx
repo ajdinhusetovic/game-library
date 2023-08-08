9695460a3c209c30ad5618ccf69f650576796d62
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-08-08 23:04:21 +0200

    a

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import './navbar.scss'
 import { Link } from "react-router-dom"
-import companyLogo from "../../assets/logo.png"
+import companyLogo from "../../assets/logo.svg"
 
 export const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="logo-container">
-                <img src={companyLogo} alt="" width={100} />
+                <img src={companyLogo} alt="" width={100} id='logo'/>
             </div>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/browse"}>Browse</Link>
-            <Link to={"/search"}>Search</Link>
-            <Link to={"/about"}>About</Link>
+            <div className="navbar-links">
+                <Link to={"/"}>Home</Link>
+                <Link to={"/browse"}>Browse</Link>
+                <Link to={"/search"}>Search</Link>
+                <Link to={"/about"}>About</Link>
+            </div>
         </div>
     )
 }
