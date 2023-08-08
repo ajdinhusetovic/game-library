b8d9e27fff8679c905cf9ae2e27c453cb2f9be20
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-08-08 22:17:26 +0200

    Add two basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import './app.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navbar } from './components/navbar/Navbar'
+import { Home } from './pages/home/Home'
+import { Browse } from './pages/browse/Browse'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/browse' element={<Browse />}></Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
