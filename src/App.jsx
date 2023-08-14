ca8600dbb58d9562b88e91f8840632301b094693
ajdinhusetovic <ajdinhusetovic@outlook.com>
2023-08-14 15:09:02 +0200

    Create game cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,25 @@ import { Home } from './pages/home/Home'
 import { Browse } from './pages/browse/Browse'
 import { Search } from './pages/search/Search'
 import { About } from './pages/about/About'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 function App() {
 
+  const client = new QueryClient()
+
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/browse' element={<Browse />}></Route>
-          <Route path='/search' element={<Search />}></Route>
-          <Route path='/about' element={<About />}></Route>
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/browse' element={<Browse />}></Route>
+            <Route path='/search' element={<Search />}></Route>
+            <Route path='/about' element={<About />}></Route>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,31 @@
 import { AnimatedPage } from '../../components/navbar/AnimatedPage'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import '../../scss/home.scss'
 
 export const Home = () => {
+
+  const { data, isLoading } = useQuery(["game"], () => {
+    return axios.get("https://www.cheapshark.com/api/1.0/deals").then((res) => res.data)
+  })
+
+  console.log(data)
+
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <AnimatedPage>
-      <div className='container'>
-        <p>Home</p>
+      <div className='container home'>
+        {data.map((game) => {
+          return <div className='game-card'>
+            <div><img src={game.thumb} width={100} /></div>
+            <div className='title'><a href={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`} className='game-link'>{game.title}</a></div>
+            <div className='price'>$<span id='normal-price'>{game.normalPrice}</span> - ${game.salePrice}</div>
+            <div>Deal rating: {game.dealRating}</div>
+          </div>
+        })}
       </div>
     </AnimatedPage>
   )
