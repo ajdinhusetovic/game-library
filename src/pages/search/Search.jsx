93ae665f7e53a701d7694b36138486a2b6e7f2e0
ajdin <ajdinhusetovic@outlook.com>
2023-08-17 20:44:24 +0200

    Add basic functionality to search

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,15 +1,43 @@
+import { useState } from 'react'
 import { AnimatedPage } from '../../components/navbar/AnimatedPage'
 import '../../scss/search.scss'
 import axios from 'axios'
 import { useQuery } from 'react-query'
 
 export const Search = () => {
+
+  const [gameTitle, setGameTitle] = useState("")
+
+  const { data, isLoading, refetch } = useQuery(["game"], () => {
+    return axios.get(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`).then((res) => res.data)
+  })
+
+  const handleInput = (e) => {
+    setGameTitle(e.target.value)
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.code === "Enter") {
+      refetch()
+    }
+  }
+
+  console.log(data)
+
+
   return (
     <AnimatedPage>
       <div className='container'>
         <div className='input-wrapper'>
           <h2>Search for a specific title</h2>
-          <input type="text" />
+          <input type="text" onChange={handleInput} onKeyDown={handleKeyDown}/>
+          <button onClick={refetch}>refetch</button>
+        </div>
+
+        <div className='games-wrapper'>
+          {data.map((game) => {
+            return <h1>{game.external}</h1>
+          })}
         </div>
       </div>
     </AnimatedPage>
