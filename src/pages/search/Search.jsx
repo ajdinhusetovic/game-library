4ad65ec7278518e1f58a07ccdddb134dae07e683
ajdin <ajdinhusetovic@outlook.com>
2023-08-17 20:13:32 +0200

    Create search text and input field

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,10 +1,14 @@
 import { AnimatedPage } from '../../components/navbar/AnimatedPage'
+import '../../scss/search.scss'
 
 export const Search = () => {
   return (
     <AnimatedPage>
       <div className='container'>
-        <p>Search</p>
+        <div className='input-wrapper'>
+          <h2>Search for a specific title</h2>
+          <input type="text" />
+        </div>
       </div>
     </AnimatedPage>
   )
