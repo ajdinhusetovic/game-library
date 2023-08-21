eeabf1145e18f3b05cde6f884e2738dee4222fd9
ajdin <ajdinhusetovic@outlook.com>
2023-08-21 16:51:42 +0200

    Add next and previous page functionality to browse page

--- src/pages/browse/Browse.jsx
+++ src/pages/browse/Browse.jsx
@@ -1,11 +1,49 @@
-import { AnimatedPage } from '../../components/navbar/AnimatedPage'
+import { useQuery } from 'react-query';
+import { AnimatedPage } from '../../components/navbar/AnimatedPage';
+import axios from 'axios';
+import { useState } from 'react';
+import { Spinner } from '../../components/navbar/Spinner';
 
 export const Browse = () => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const { data, isLoading, refetch } = useQuery(['game', pageNumber], () => {
+    return axios.get(`https://www.cheapshark.com/api/1.0/deals?pageNumber=${pageNumber}`)
+      .then((res) => res.data);
+  });
+
+  const handleNextPage = () => {
+    setPageNumber(pageNumber + 1);
+    refetch();
+  };
+
+  const handlePreviousPage = () => {
+    if (pageNumber === 0) {
+      alert("Error")
+    } else {
+      setPageNumber(pageNumber - 1)
+      refetch();
+    }
+  };
+
   return (
     <AnimatedPage>
       <div className='container'>
         <p>Browse</p>
+        <button onClick={handleNextPage}>Next Page</button>
+        <button onClick={handlePreviousPage}>prev Page</button>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div>
+            {data.map(item => (
+              <>
+                <h1>{item.title}</h1>
+              </>
+            ))}
+          </div>
+        )}
       </div>
     </AnimatedPage>
-  )
-}
+  );
+};
